c5ddbe9c0bebd1131de7761a23aabe436026fa09
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-03-20 14:12:34 +0800

    项目初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+</script>
+
+<template>
+    <div class="container">
+      <n-space>
+        <n-layout>
+          <n-layout-header>颐和园路</n-layout-header>
+          <n-layout-content content-style="padding: 24px;">
+            平山道
+          </n-layout-content>
+          <n-layout-footer>成府路</n-layout-footer>
+        </n-layout>
+      </n-space>
+    </div>
+</template>
