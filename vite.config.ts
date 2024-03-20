3d77832c57ff31eb1c930e2df11c709fb8fd409e
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-03-20 14:12:34 +0800

    迁移代码

---
+++ vite.config.ts
@@ -0,0 +1,138 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import VueRouter from 'unplugin-vue-router/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({}),
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        // custom
+        {
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'] // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          'axios': [
+            // default imports
+            ['default', 'axios'] // import { default as axios } from 'axios',
+          ],
+          'naive-ui': [],
+          '[package-name]': [
+            '[import-names]',
+            // alias
+            ['[from]', '[alias]']
+          ]
+        },
+        // example type import
+        {
+          from: 'vue-router',
+          imports: ['RouteLocationRaw'],
+          type: true
+        }
+      ],
+
+      // Array of strings of regexes that contains imports meant to be filtered out.
+      ignore: [
+        'useMouse',
+        'useFetch'
+      ],
+
+      // Enable auto import by filename for default module exports under directories
+      defaultExportByFilename: false,
+
+      // Auto import for module exports under directories
+      // by default it only scan one level of modules under the directory
+      dirs: [
+        // './hooks',
+        // './composables' // only root modules
+        // './composables/**', // all nested modules
+        // ...
+      ],
+
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: './auto-imports.d.ts',
+
+      // Array of strings of regexes that contains imports meant to be ignored during
+      // the declaration file generation. You may find this useful when you need to provide
+      // a custom signature for a function.
+      ignoreDts: [
+        'ignoredFunction',
+        /^ignore_/
+      ],
+
+      // Auto import inside Vue template
+      // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
+      vueTemplate: false,
+
+      // Custom resolvers, compatible with `unplugin-vue-components`
+      // see https://github.com/antfu/unplugin-auto-import/pull/23/
+      resolvers: [
+        /* ... */
+      ],
+
+      // Inject the imports at the end of other imports
+      injectAtEnd: true,
+
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      }
+    }),
+    Components({
+      dts: true,
+      types: [
+        {
+          from: 'vue-router',
+          names: ['RouterLink', 'RouterView']
+        }
+      ],
+      // relative paths to the directory to search for components.
+      dirs: ['src/components'],
+      resolvers: [
+        NaiveUiResolver()
+      ],
+      // valid file extensions for components.
+      extensions: ['vue'],
+      // filters for transforming targets
+      include: [/\.vue$/, /\.vue\?vue/],
+      exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
+      deep: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+})
