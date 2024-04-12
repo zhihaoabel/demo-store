c83eb47520f301dba2250b44b65a64e54cb9098d
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-04-12 15:31:48 +0800

    修改环境变量和proxy

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  // baseURL: 'https://demo-store-one.vercel.app',
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
   timeout: 10000
 })
 
