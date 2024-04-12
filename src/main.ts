5e7d328a48d7744869e8231791c953987605a9be
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-04-12 16:03:57 +0800

    修改script

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 console.log(import.meta.env.VITE_APP_API_BASE_URL, 'base url')
+console.log(import.meta.env.VITE_APP_TITLE, 'app name')
+
 app.mount('#app')
