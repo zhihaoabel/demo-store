e55adeee911b8be072c98dba7fc00524e0e60d17
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-04-16 09:58:55 +0800

    去掉baseURL

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_APP_API_BASE_URL,
   timeout: 10000
 })
 
