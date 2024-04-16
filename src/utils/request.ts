417267166374ca5299d9bc6f6e91e2f4400d7043
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-04-16 11:37:33 +0800

    测试跨域问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  // baseURL: 'https://sandbox-v3-acquiring.pacypay.com/',
+  baseURL: 'http://localhost:5173/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
