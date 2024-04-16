5b1a630bef3222fa181af2bca196dc89b0b46375
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-04-16 11:45:16 +0800

    测试跨域问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'https://demo-store-one.vercel.app/',
+  baseURL: 'http://localhost:5173/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
