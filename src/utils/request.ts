31e62494199cd67deabb97c344899d870b417988
zhihaoabel <61072512+zhihaoabel@users.noreply.github.com>
2024-04-18 10:49:08 +0800

    测试跨域问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,9 +3,6 @@ import axios from 'axios'
 const request = axios.create({
   // baseURL: '/',
   timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json'
-  }
 })
 
 request.interceptors.request.use((config) => {
